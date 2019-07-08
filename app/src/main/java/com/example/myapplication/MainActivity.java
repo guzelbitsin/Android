a956fae3194d542029fc7dacb8c1fdfc35192507
barisonen <barisonen96@hotmail.com>
2019-07-08 20:58:51 +0300

    main page updated, searching user interface and page added

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -24,4 +24,9 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void search(View view) {
+        Intent intent = new Intent( this, SearchUsersResult.class);
+        startActivity(intent);
+    }
+
 }
