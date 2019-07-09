8cc4b427ddb034b5626686b2c134996d4543db82
Sukran Saygılı <sukransaygili@Sukran-MacBook-Pro.local>
2019-07-09 22:39:46 +0300

    Created Android Rest using Spring Rest

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,17 +1,32 @@
 package com.example.myapplication;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 import android.content.Intent;
+import android.widget.Button;
+
 public class MainActivity extends AppCompatActivity {
 
+    Button btnCallRestApi;
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainactivity);
+
+        btnCallRestApi = (Button) findViewById(R.id.btnCallRestApi);
+        btnCallRestApi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //call rest api
+
+            }
+        });
     }
 
     public void createNewRecipe(View view) {
