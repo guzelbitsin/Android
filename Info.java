eeb3c03b77a160ce824c71a69e41cdb0e7a01764
Sukran Saygılı <sukransaygili@Sukran-MacBook-Pro.local>
2019-07-10 21:59:25 +0300

    rest db info added

---
+++ Info.java
@@ -0,0 +1,34 @@
+
+public class User {
+
+private String id;
+private String tag;
+private String name;
+private String description;
+private String picture;
+private String emil;
+
+public String getId() {
+return this.id;
+}
+
+public String getTag() {
+return this.tag;
+}
+
+public String getName() {
+return this.name;
+}
+
+public String getDescription() {
+        return this.description;
+    }
+
+public String getPicture() {
+return this.picture;
+}
+
+public String getEmail() {
+return this.emil;
+}
+}
